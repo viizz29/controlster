9943a50e9b8639c4fa556a62e4f9435c421f1652
bp7788 <viizzasus@gmail.com>
2026-09-25 14:44:40 +0000

    fixed the frontend base name issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,31 @@
+import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_NAME || "/",
-  optimizeDeps: {
-    include: ["lottie-react"],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  console.log("VITE_BASE_NAME:", env.VITE_BASE_NAME);
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_NAME || "/",
+    optimizeDeps: {
+      include: ["lottie-react"],
     },
-  },
 
-  // 👇 ADD THIS
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/setup-tests.ts",
-    globals: true,
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+
+    // 👇 ADD THIS
+    test: {
+      environment: "jsdom",
+      setupFiles: "./src/setup-tests.ts",
+      globals: true,
+    },
+  };
 });
